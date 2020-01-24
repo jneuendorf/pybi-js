cd3e92796d59b21aa5cc03ce349826bbf4ae419e
Jim Neuendorf <jim.neuendorf@gmx.de>
2020-01-24 21:52:56 +0100

    first tests, fixed pow

---
+++ __tests__/math.js
@@ -0,0 +1,133 @@
+const abs = require('../src/abs')
+const divmod = require('../src/divmod')
+const max = require('../src/max')
+const min = require('../src/min')
+const pow = require('../src/pow')
+const round = require('../src/round')
+const sum = require('../src/sum')
+
+
+describe('abs', () => {
+    test('simple', () => {
+        const args = [
+            1,
+            -2,
+            'a',
+            Infinity,
+            -Infinity,
+        ]
+        for (const arg in args) {
+            expect(abs.simple(arg)).toBe(Math.abs(arg))
+        }
+    })
+
+    test('extended', () => {
+        const right = {
+            __abs__() {
+                return 'surprise!'
+            }
+        }
+        const wrong = {__abs__: 'surprise!'}
+        expect(abs.extended(right)).toBe('surprise!')
+        expect(() => abs.extended(wrong)).toThrow()
+    })
+})
+
+
+describe('divmod', () => {
+    test('simple', () => {
+        const args = [
+            1,
+            -2,
+            'a',
+            Infinity,
+            -Infinity,
+        ]
+        for (const arg in args) {
+            expect(abs.simple(arg)).toBe(Math.abs(arg))
+        }
+    })
+
+    test('extended', () => {
+        const right = {
+            __abs__() {
+                return 'surprise!'
+            }
+        }
+        const wrong = {__abs__: 'surprise!'}
+        expect(abs.extended(right)).toBe('surprise!')
+        expect(() => abs.extended(wrong)).toThrow()
+    })
+})
+
+
+describe('max', () => {
+    test('with iterable', () => {
+        const args2d = [
+            [1, 3, 7, 1, -2],
+            ['a', 2],
+            [Infinity, 2**20],
+        ]
+        for (const args in args2d) {
+            expect(max(args)).toBe(Math.max(...args))
+        }
+    })
+
+    test('with multiple args', () => {
+        const args2d = [
+            [1, 3, 7, 1, -2],
+            ['a', 2],
+            [Infinity, 2**20],
+        ]
+        for (const args in args2d) {
+            expect(max(...args)).toBe(Math.max(...args))
+        }
+    })
+})
+
+
+describe('min', () => {
+    test('with iterable', () => {
+        const args2d = [
+            [1, 3, 7, 1, -2],
+            ['a', 2],
+            [Infinity, 2**20],
+        ]
+        for (const args in args2d) {
+            expect(min(args)).toBe(Math.min(...args))
+        }
+    })
+
+    test('with multiple args', () => {
+        const args2d = [
+            [1, 3, 7, 1, -2],
+            ['a', 2],
+            [Infinity, 2**20],
+        ]
+        for (const args in args2d) {
+            expect(min(...args)).toBe(Math.min(...args))
+        }
+    })
+})
+
+
+describe('pow', () => {
+    test('without mod arg', () => {
+        const args2d = [
+            [1, 3],
+            [1, 0],
+            [0, 30],
+            ['a', 2],
+            [Infinity, 2],
+            [-Infinity, 2],
+        ]
+        for (const args in args2d) {
+            expect(pow(args)).toBe(Math.pow(...args))
+        }
+    })
+
+    test('with mod arg', () => {
+        // See example at https://docs.python.org/3/library/functions.html#pow
+        expect(pow(38, -1, 97)).toBe(23)
+    })
+})
--- src/pow.js
+++ src/pow.js
@@ -1,9 +1,57 @@
 const _mod = require('./_mod')
+const {ValueError} = require('./_errors')
+const divmod = require('./divmod')
 
-module.exports = (base, exp, mod) => {
+
+// See https://bugs.python.org/msg336130
+function modinv(a, m) {  // assuming m > 0
+    let b = m, s = 1, s1 = 0, q
+
+    while (b) {
+        [a, [q, b]] = [b, divmod(a, b)]
+        // NOTE: Semicolon in the beginning to make clear it is intended.
+        //       See: https://stackoverflow.com/a/38050480/6928824
+        ;[s, s1] = [s1, s - q*s1]
+    }
+    if (a !== 1) {
+        throw new ValueError('inverse does not exist')
+    }
+    return (
+        s >= 0
+        ? s
+        : s + m
+    )
+}
+
+function pow(base, exp, mod) {
     const n = base ** exp
-    if (!mod) {
+    if (mod == null) {
         return n
     }
-    return _mod(n, mod)
+
+    if (mod === 0) {
+        throw new ValueError('pow() arg 3 must not be zero')
+    }
+
+    if (exp >= 0) {
+        return _mod(n, mod)
+    }
+    else {
+        let invBase
+        try {
+            invBase = modinv(base, mod)
+        }
+        catch (error) {
+            if (error instanceof ValueError) {
+                throw new ValueError(`pow() arg 1 and arg 3 must be relatively prime`)
+            }
+            else {
+                throw error
+            }
+        }
+        return pow(invBase, -exp, mod)
+    }
 }
+
+
+module.exports = pow
