e2f88bd03b72b358bc96b09b44aa6d05c4465b3c
Jim Neuendorf <jim.neuendorf@gmx.de>
2020-01-27 22:41:43 +0100

    more tests, fixed filter

--- __tests__/iterables.js
+++ __tests__/iterables.js
@@ -16,4 +16,22 @@ const {createTestCase} = require('./_utils')
 
 createTestCase('iterables', 'all', all)
 createTestCase('iterables', 'any', any)
-createTestCase('iterables', 'enumerate', enumerate, {logIndices: true})
+createTestCase('iterables', 'enumerate', enumerate)
+
+describe('filter', () => {
+    test('without function argument', () => {
+        expect([...filter([-1, 0, 1, 2])]).toEqual([-1, 1, 2])
+        expect([...filter([-1, 1, 2])]).toEqual([-1, 1, 2])
+        expect([...filter([0, true, false, 'not empty'])]).toEqual([true, 'not empty'])
+        expect([...filter([0, false, [], {}, ''])]).toEqual([])
+    })
+
+    test('with function argument', () => {
+        expect([...filter(x => x < 1, [-1, 0, 1, 2])]).toEqual([-1, 0])
+        expect([...filter(x => x >= 1, [-1, 0, 1, 2])]).toEqual([1, 2])
+    })
+
+    test('with wrong number of arguments', () => {
+        expect(() => [...filter(1, 2 ,3)]).toThrow(TypeError)
+    })
+})
--- src/filter.js
+++ src/filter.js
@@ -1,7 +1,20 @@
 const bool = require('./bool')
 
 
-function* filter(iterable, predicate=bool) {
+function* filter(...args) {
+    let iterable, predicate
+    if (args.length === 2) {
+        [predicate, iterable] = args
+    }
+    else if (args.length === 1) {
+        predicate = bool
+        ;[iterable] = args
+    }
+    else {
+        throw new TypeError(
+            `filter() takes 1 or 2 arguments but ${args.length} were given`
+        )
+    }
     for (const item of iterable) {
         if (predicate(item)) {
             yield item
