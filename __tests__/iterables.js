2b89fb6593b39520668e7598229a9d82c11d6d6b
Jim Neuendorf <jim.neuendorf@gmx.de>
2020-01-28 23:15:53 +0100

    zip

--- __tests__/iterables.js
+++ __tests__/iterables.js
@@ -165,3 +165,19 @@ describe('sorted', () => {
         )
     })
 })
+
+
+// Coverage already 100% through the other functions.
+describe('zip', () => {
+    test('simple', () => {
+        expect([...zip(
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9, 10]
+        )]).toEqual([
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9]
+        ])
+    })
+})
