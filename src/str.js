9367ed21985858478ad8537f8f2787a4c93d6a75
Jim Neuendorf <jim.neuendorf@gmx.de>
2020-02-10 22:42:11 +0100

    WIP: str

--- src/str.js
+++ src/str.js
@@ -1,7 +1,40 @@
-module.exports = object => {
-    if (object.__str__) {
-        return object.__str__()
+const {ValueError} = require('./_errors')
+const toPrimitive = require('./_to-primitive')
+
+module.exports = (...args) => {
+    if (args.length === 0) {
+        return ''
     }
+
+    let [object, encoding, /*errors*/] = args
+
+    if (object == null) {
+        return `${$object}`
+    }
+
+    if (!encoding) {
+        // From the Python docs:
+        // If neither encoding nor errors is given,
+        // str(object) returns object.__str__(). [...]
+        // For string objects, this is the string itself.
+        try {
+            if (typeof(toPrimitive(object)) === 'string') {
+                return object
+            }
+        }
+        catch (error) {
+            if (error instanceof ValueError) {
+                if (object.__str__) {
+                    return object.__str__()
+                }
+            }
+        }
+    }
+    else {
+
+    }
+
+
 
     // No 'new' => primitive string
     // (see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Distinction_between_string_primitives_and_String_objects)
